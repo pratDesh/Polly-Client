5c406e1dbb626b106208c310c656aee1405333e4
pdeshmukh <pdeshmukh@mediaocean.com>
2021-01-05 20:48:52 +0530

    initial commit for Login/signup page

--- quiz-app/src/user/signup/Signup.js
+++ quiz-app/src/user/signup/Signup.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import './Signup.css';
 
 class Signup extends Component{
-
+    
     render(){
         return (
             <div className="signup-container">
